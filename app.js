c5a10dce813cecfb94ee7cc5d7d5c94f287b875e
Martin <martin.ostrat@khk.ee>
2021-11-04 23:15:01 +0200

    Add books to the table

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
-function add() {
+function addBook() {
+    const bookTitle = document.getElementById('title').value;
+    const bookAuthor = document.getElementById('author').value;
+    const bookIsbn = document.getElementById('isbn').value;
+    let btn = document.createElement('button');
+    btn.innerHTML = 'X';
+    btn.addEventListener('click', function() {
+        console.log('del');
+    }, false);
 
-}
 
-function del() {
+    const bookTable = document.getElementById('bTable');
+    let row = bookTable.insertRow(-1);
+    let cell1 = row.insertCell(0);
+    let cell2 = row.insertCell(1);
+    let cell3 = row.insertCell(2);
+    let cell4 = row.insertCell(3);
+
+
+    cell1.innerHTML = bookTitle;
+    cell2.innerHTML = bookAuthor;
+    cell3.innerHTML = bookIsbn;
+    cell4.appendChild(btn);
     
 }
